8aebad7a60bb1e9328f47de233789499dd484cec
beamlakchernetschool <beamlak.chernet@aastustudent.edu.et>
2025-08-09 21:17:02 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const modeToggle = document.getElementById('mode-toggle');
-
-modeToggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
-    modeToggle.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
-});
-
